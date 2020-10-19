a89bcfc517562f524c885d8b0b699c9af6a45dea
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-19 03:50:33 +0200

    Create task7.py

---
+++ task7.py
@@ -0,0 +1,18 @@
+#Celsius to Fahrenheit
+
+num1 = int(input("enter temperature in degrees celsius: "))
+
+degreesFah = str((num1 * 9/5) + 32)
+
+print("The temperature in degrees fahrenheit is " + degreesFah)
+
+
+###############################################################
+
+
+#Fahrenheit to Celsius
+num1 = int(input("enter temperature in degrees fahrenheit: "))
+
+degreesCel = str((num1 - 32) * 5/9)
+
+print("The temperature in degrees celsius is " + degreesCel)
