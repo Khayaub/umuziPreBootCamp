84a16ad380d909b232c101af52712c5ed577204b
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-19 02:20:01 +0200

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,12 @@
+x = 1 + 1 * 2 #multiplication before addition(1 * 2) = 2, then 2 + 1
+y = (1 + 1) * 2 #brackets first(1 + 1)= 2, then 2 * 2
+z = 1 + (1 * 2) #brackets first(1 * 2)= 2, then 2 + 1
+a = 1 + 1 * 2/2 #division before addition (2 / 2) = 1, then 2 * 2 from (1 + 1)
+b = (1 + 1 * 2)/2#brackets first, in brackets multiplication before addition(1 * 2) = 2, then 2 + 1 = 3. bracket / 2
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+
