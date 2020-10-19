bdfcec56d2b1964cfa9d46b5c949498bd56a7475
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-19 17:44:35 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,13 @@
-num1 = int(input("enter 1st number: "))
-num2 = int(input("enter 2nd number: "))
+def true65(x,y):
+    if x ==65 or y == 65 or (x + y) == 65:
+       print("True")
+    else:
+        print("False")
 
-if (num1 or num2 == 65) or ((num1 + num2) == 65):
-    print("True")
-else:
-    print("False")
+#testing
+#true65(65,30)
+#true65(5,6)
+#true65(70,-5)
+#true65(35,30)
+#true65(-10,75)
+#true65(10,75)
