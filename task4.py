66e0d0cc6266d7a1b73ee8ea64b0311365d80237
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-19 02:58:38 +0200

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,5 +1,6 @@
-num1 = int(input())
-num2 = int(input())
+
+num1 = int(input("enter 1st number: "))
+num2 = int(input("enter 2nd number: "))
 sumNums = str(num1 + num2)
 
 #print(sumNums), printed sumNums for testing
