2493e7634a57be9bde4a012ddb9775ce306065ec
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-20 13:12:35 +0200

    Update task8.py

--- task8.py
+++ task8.py
@@ -1,9 +1,17 @@
+def minutesToHours(x):
+    hours = x//60
+    minutes = x%60
+    numAns = str(x)
 
-num1 = int(input("enter number of minutes: "))
+    print(numAns + " minutes converts to " + str(hours) + " hour(s) and "
+          + str(minutes) + " minutes ")
 
-hours = num1//60
-minutes = num1%60
 
-numAns = str(num1)
 
-print(numAns + " minutes converts to " + str(hours) + " hour(s) and " + str(minutes) + " minutes")
+# Test  
+#minutesToHours(71)
+#minutesToHours(133)
+#minutesToHours(58)
+#minutesToHours(240)
+#minutesToHours(90)
+#minutesToHours(999)
