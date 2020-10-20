d23586b6fdc36c7124c1b3492158743564637c5b
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-20 14:49:28 +0200

    Update task11.py

--- task11.py
+++ task11.py
@@ -1,12 +1,11 @@
-myString1 = input("enter 1st string: " )
-myString2 = input("enter 2nd string: " )
 
-combinedString = myString1 + myString2
+def commonLetters():
+    string1 = input("Enter 1st string: ")
+    string2 = input("Enter 2nd string: ")
+    combinedString = string1 + string2
 
-#print(combinedString)
+    for letters in combinedString:
+        if letters in string1 and letters in string2:
+            print(letters)
 
-for letters in combinedString:
-    if letters in myString1 and letters in myString2:
-        print(letters)
-        
-
+commonLetters()
