051d501736358deb5592552264287be34d4b6435
Khaya Ubisi <36042049+Khayaub@users.noreply.github.com>
2020-10-21 16:09:28 +0200

    Update task9.py

--- task9.py
+++ task9.py
@@ -10,8 +10,8 @@ def sumUntil(x,y,z):
     print("The sum of all natural number multiples of " + str(x) + " and "
           + str(y) + " below " + str(z) + " is " + Sum)
     
+sumUntil(3,5,10)
 
 #Test    
-#sumUntil(3,5,10)
 #sumUntil(3,5,1000)
 #sumUntil(2,3,10)
